7c04826766286fb3466b0788cb5ca765b6175b64
vintots <mnoche01@gmail.com>
2017-07-12 16:49:37 +0800

    SetUP dev Environment

--- app/app.js
+++ app/app.js
@@ -2,13 +2,18 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  // 'ngRoute',	
+  // 'myApp.view1',
+  // 'myApp.view2',
+  'ui.router',
+  'ngMaterial',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+.config(function($stateProvider, $urlRouterProvider) {
+	 $urlRouterProvider.otherwise('/login')
+})
+
+.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}])
---
+++ app/modules/login/login-state.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var myApp = angular.module('myApp');
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+
+	 $stateProvider
+    
+    .state('login', {
+     url: "/login",
+     cache: false,
+     templateUrl: "modules/login/login.html",
+     // controller: 'testAwsController'
+   })
+})
